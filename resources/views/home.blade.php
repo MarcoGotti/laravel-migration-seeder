e8dba884d59377027951e14623312f1510cc0ad8
MarcoGotti <mrc.gotti@gmail.com>
2024-05-25 11:19:07 +0000

    add update_table;

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,6 @@
 
 @section('content')
 
-ciao
+
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Train;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $trains = Train::all();
+
+    return view('home', compact('trains'));
 });
